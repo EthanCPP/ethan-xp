45260b86ac0bc2cc4238e2e530e9caca1aa5188f
Ethan <cppethan@gmail.com>
2024-04-20 23:47:28 +0100

    iframe fix?

--- resources/js/Components/Applications/ExplorerLocations/InternetExplorer.vue
+++ resources/js/Components/Applications/ExplorerLocations/InternetExplorer.vue
@@ -3,5 +3,5 @@ const props = defineProps(['address']);
 </script>
 
 <template>
-    <iframe :src="address" style="width: 100%; height: 100%;"></iframe>
+    <iframe :src="address" style="width: 100%; height: 100%;" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </template>
